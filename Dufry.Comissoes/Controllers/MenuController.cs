60401b82d7ecead23060856f27ba21fa8b6c3c6b
Roberto Hermes <robertohermes@gmail.com>
2015-07-30 15:39:54 -0300

    Iplementação inicial do controle de acesso

--- Dufry.Comissoes/Controllers/MenuController.cs
+++ Dufry.Comissoes/Controllers/MenuController.cs
@@ -3,34 +3,23 @@ using Dufry.Comissoes.Application.Interfaces;
 
 namespace Dufry.Comissoes.Controllers
 {
-    public class HomeController : Controller
+    public class MenuController : Controller
     {
         private readonly IControleAcessoAppService _controleacessoAppService;
 
-        public HomeController(IControleAcessoAppService controleacessoAppService)
+        public MenuController(IControleAcessoAppService controleacessoAppService)
         {
             _controleacessoAppService = controleacessoAppService;
         }
 
-        //
-        // GET: /Home/Index
-        public ActionResult Index()
+        // GET: /Home/SideMenu
+        public ActionResult SideMenu()
         {
             string login = _controleacessoAppService.ObtainCurrentLoginFromAd();
 
             var controleacessoModel = _controleacessoAppService.FindByActiveLogin(login);
 
-            return View(controleacessoModel);
-        }
-
-        //
-        // GET: /ControleAcesso/Browse
-
-        public ActionResult Browse()
-        {
-            var controleacessoModel = _controleacessoAppService.GetFirstLogin();
-
-            return View(controleacessoModel);
+            return PartialView(controleacessoModel);
         }
 
     }
