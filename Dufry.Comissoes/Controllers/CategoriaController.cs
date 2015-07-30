d81a81a1c843476d92f1401cc937d654d15f764e
Roberto Hermes <robertohermes@gmail.com>
2015-07-30 17:58:10 -0300

    Iplementação inicial da estrutura de administração de Categorias

--- Dufry.Comissoes/Controllers/CategoriaController.cs
+++ Dufry.Comissoes/Controllers/CategoriaController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Dufry.Comissoes.Application.Interfaces;
+using PagedList;
 
 namespace Dufry.Comissoes.Controllers
 {
@@ -24,6 +25,53 @@ namespace Dufry.Comissoes.Controllers
 
 
             return View(categoriaModel);
+        }
+
+        // GET: /Categoria/CategoriaIndex
+        public ViewResult CategoriaIndex(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "DESC_CATEGORIA_desc" : "";
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "COL_ORIGEM_desc" : "";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var categorias = _categoriaAppService.All();
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                categorias = categorias.Where(s => s.DESC_CATEGORIA.Contains(searchString)
+                                       || s.TAB_ORIGEM.Contains(searchString)
+                                       || s.COL_ORIGEM.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "DESC_CATEGORIA_desc":
+                    categorias = categorias.OrderByDescending(s => s.DESC_CATEGORIA);
+                    break;
+                case "COL_ORIGEM":
+                    categorias = categorias.OrderBy(s => s.COL_ORIGEM);
+                    break;
+                case "COL_ORIGEM_desc":
+                    categorias = categorias.OrderByDescending(s => s.COL_ORIGEM);
+                    break;
+                default:  // DESC_CATEGORIA ascending 
+                    categorias = categorias.OrderBy(s => s.DESC_CATEGORIA);
+                    break;
+            }
+
+            int pageSize = 3;
+            int pageNumber = (page ?? 1);
+            return View(categorias.ToPagedList(pageNumber, pageSize));
         }
 
     }
