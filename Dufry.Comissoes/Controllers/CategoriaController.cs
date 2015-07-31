b651bd2e6c1aaa7165c427dcfad2257b43f30fde
Roberto Hermes <robertohermes@gmail.com>
2015-07-31 13:57:39 -0300

    Ajustes na busca de Categorias

--- Dufry.Comissoes/Controllers/CategoriaController.cs
+++ Dufry.Comissoes/Controllers/CategoriaController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Dufry.Comissoes.Application.Interfaces;
 using PagedList;
+using Dufry.Comissoes.Domain.Entities;
 
 namespace Dufry.Comissoes.Controllers
 {
@@ -17,22 +18,14 @@ namespace Dufry.Comissoes.Controllers
             _categoriaAppService = categoriaAppService;
         }
 
-
-        // GET: /Categoria/CategoriaBusca
-        public ActionResult CategoriaBusca()
-        {
-            var categoriaModel = _categoriaAppService.All();
-
-
-            return View(categoriaModel);
-        }
-
         // GET: /Categoria/CategoriaIndex
         public ViewResult CategoriaIndex(string sortOrder, string currentFilter, string searchString, int? page)
         {
             ViewBag.CurrentSort = sortOrder;
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "DESC_CATEGORIA_desc" : "";
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "COL_ORIGEM_desc" : "";
+            ViewBag.DescCategoriaSortParm = String.IsNullOrEmpty(sortOrder) ? "DESC_CATEGORIA_desc" : "";
+            ViewBag.TabOrigemSortParm = sortOrder == "TAB_ORIGEM" ? "TAB_ORIGEM_desc" : "TAB_ORIGEM";
+            ViewBag.ColOrigemSortParm = sortOrder == "COL_ORIGEM" ? "COL_ORIGEM_desc" : "COL_ORIGEM";
+            ViewBag.StatusSortParm = sortOrder == "STATUS" ? "STATUS_desc" : "STATUS";
 
             if (searchString != null)
             {
@@ -45,31 +38,48 @@ namespace Dufry.Comissoes.Controllers
 
             ViewBag.CurrentFilter = searchString;
 
-            var categorias = _categoriaAppService.All();
+            IEnumerable<Categoria> categorias = new List<Categoria>();
 
-            if (!String.IsNullOrEmpty(searchString))
+            if (String.IsNullOrEmpty(searchString))
             {
-                categorias = categorias.Where(s => s.DESC_CATEGORIA.Contains(searchString)
-                                       || s.TAB_ORIGEM.Contains(searchString)
-                                       || s.COL_ORIGEM.Contains(searchString));
+                categorias = _categoriaAppService.All();
             }
+            else
+            {
+                categorias = _categoriaAppService.Find(s => s.DESC_CATEGORIA.Contains(searchString)
+                                           || s.TAB_ORIGEM.Contains(searchString)
+                                           || s.COL_ORIGEM.Contains(searchString));
+            }
+
             switch (sortOrder)
             {
-                case "DESC_CATEGORIA_desc":
-                    categorias = categorias.OrderByDescending(s => s.DESC_CATEGORIA);
-                    break;
                 case "COL_ORIGEM":
                     categorias = categorias.OrderBy(s => s.COL_ORIGEM);
                     break;
+                case "TAB_ORIGEM":
+                    categorias = categorias.OrderBy(s => s.TAB_ORIGEM);
+                    break;
+                case "STATUS":
+                    categorias = categorias.OrderBy(s => s.STATUS);
+                    break;
+                case "DESC_CATEGORIA_desc":
+                    categorias = categorias.OrderByDescending(s => s.DESC_CATEGORIA);
+                    break;
                 case "COL_ORIGEM_desc":
                     categorias = categorias.OrderByDescending(s => s.COL_ORIGEM);
+                    break;
+                case "TAB_ORIGEM_desc":
+                    categorias = categorias.OrderByDescending(s => s.TAB_ORIGEM);
+                    break;
+                case "STATUS_desc":
+                    categorias = categorias.OrderByDescending(s => s.STATUS);
                     break;
                 default:  // DESC_CATEGORIA ascending 
                     categorias = categorias.OrderBy(s => s.DESC_CATEGORIA);
                     break;
             }
 
-            int pageSize = 3;
+            int pageSize = 10;
             int pageNumber = (page ?? 1);
             return View(categorias.ToPagedList(pageNumber, pageSize));
         }
