c5f1be86226868e6148650219d2fe8c9540142cc
Roberto Hermes <robertohermes@gmail.com>
2015-08-31 10:48:30 -0300

    Ajustes na criação e edição de Planos de Comissões e Hierarquias

--- Dufry.Comissoes/Controllers/PlanoController.cs
+++ Dufry.Comissoes/Controllers/PlanoController.cs
@@ -18,15 +18,11 @@ namespace Dufry.Comissoes.Controllers
     {
         private readonly IControleAcessoAppService _controleacessoAppService;
         private readonly IPlanoAppService _planoAppService;
-        private readonly ICategoriaAppService _categoriaAppService;
-        private readonly ILojaAppService _lojaAppService;
 
-        public PlanoController(IControleAcessoAppService controleacessoAppService, IPlanoAppService planoAppService, ICategoriaAppService categoriaAppService, ILojaAppService lojaAppService)
+        public PlanoController(IControleAcessoAppService controleacessoAppService, IPlanoAppService planoAppService)
         {
             _controleacessoAppService = controleacessoAppService;
             _planoAppService = planoAppService;
-            _categoriaAppService = categoriaAppService;
-            _lojaAppService = lojaAppService;
         }
 
         // GET: /Plano/PlanoCreate
@@ -34,15 +30,7 @@ namespace Dufry.Comissoes.Controllers
         {
             Plano plano = new Plano();
 
-            var categorias = _categoriaAppService.Find(t => t.STATUS == "A");
-            IEnumerable<SelectListItem> categoriaSelectListItem = new SelectList(categorias, "ID_CATEGORIA", "DESC_CATEGORIA");
-            ViewBag.ID_CATEGORIA = new SelectList(categorias, "ID_CATEGORIA", "DESC_CATEGORIA");
-
-            var lojas = _lojaAppService.Find(t => t.CodigoLojaAlternate.Trim() != "-2" && t.CodigoLojaAlternate.Trim() != "-1"); ;
-            IEnumerable<SelectListItem> lojaSelectListItem = new SelectList(lojas, "CodigoLojaAlternate", "NomeLoja");
-            ViewBag.CODIGOLOJAALTERNATE = new SelectList(lojas, "CodigoLojaAlternate", "NomeLoja");
-
-            PlanoViewModel planoVM = new PlanoViewModel(plano, categoriaSelectListItem, lojaSelectListItem);
+            PlanoViewModel planoVM = new PlanoViewModel(plano);
 
             return View(planoVM);
         }
@@ -155,6 +143,146 @@ namespace Dufry.Comissoes.Controllers
             }
 
             return View(planoToUpdate);
+        }
+
+        // GET: /Plano/PlanoIndex
+        //[ControleAcessoAdminFilter]
+        public ViewResult PlanoIndex(int? page
+            , string sortOrder
+            , string descplanoSearchString
+            , string dtiniSearchString
+            , string dtfimSearchString
+            , string statusSearchString)
+        {
+
+            #region trataParametrosOrdenacao
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.DescPlanoSortParam = String.IsNullOrEmpty(sortOrder) ? "DESC_PLANO" : "";
+            ViewBag.DtIniSortParam = sortOrder == "DT_INI" ? "DT_INI_desc" : "DT_INI";
+            ViewBag.DtFimSortParam = sortOrder == "DT_FIM" ? "DT_FIM_desc" : "DT_FIM";
+            ViewBag.StatusSortParam = sortOrder == "STATUS" ? "STATUS_desc" : "STATUS";
+            #endregion trataParametrosOrdenacao
+
+            #region trataParametrosBusca
+
+            var predicate = PredicateBuilder.True<Plano>();
+
+            if (!String.IsNullOrEmpty(descplanoSearchString))
+            {
+                predicate = predicate.And(i => i.DESC_PLANO.Contains(descplanoSearchString));
+                ViewBag.descplanoFilter = descplanoSearchString;
+            }
+
+            if (!String.IsNullOrEmpty(dtiniSearchString))
+            {
+                DateTime dtiniFilter = DateTime.ParseExact(dtiniSearchString, "dd/MM/yyyy", new CultureInfo("pt-BR"));
+                predicate = predicate.And(i => i.DT_INI.Equals(dtiniFilter));
+                ViewBag.dtiniFilter = dtiniFilter;
+            }
+
+            if (!String.IsNullOrEmpty(dtfimSearchString))
+            {
+                DateTime dtfimFilter = DateTime.ParseExact(dtfimSearchString, "dd/MM/yyyy", new CultureInfo("pt-BR"));
+                predicate = predicate.And(i => i.DT_FIM.Equals(dtfimFilter));
+                ViewBag.dtfimFilter = dtfimFilter;
+            }
+
+            if (!String.IsNullOrEmpty(statusSearchString))
+            {
+                predicate = predicate.And(i => i.STATUS.Equals(statusSearchString));
+                ViewBag.statusFilter = statusSearchString;
+            }
+
+            #endregion trataParametrosBusca
+
+            IEnumerable<Plano> planos = new List<Plano>();
+
+            planos = _planoAppService.Find(predicate);
+
+            #region ordenacao
+            switch (sortOrder)
+            {
+                case "DT_INI":
+                    planos = planos.OrderBy(s => s.DT_INI); //mudar de chave para campo
+                    break;
+                case "DT_FIM":
+                    planos = planos.OrderBy(s => s.DT_FIM); //mudar de chave para campo
+                    break;
+                case "STATUS":
+                    planos = planos.OrderBy(s => s.STATUS);
+                    break;
+                case "DESC_PLANO_desc":
+                    planos = planos.OrderByDescending(s => s.DESC_PLANO);
+                    break;
+                case "DT_INI_desc":
+                    planos = planos.OrderByDescending(s => s.DT_INI); //mudar de chave para campo
+                    break;
+                case "DT_FIM_desc":
+                    planos = planos.OrderByDescending(s => s.DT_FIM); //mudar de chave para campo
+                    break;
+                case "STATUS_desc":
+                    planos = planos.OrderByDescending(s => s.STATUS);
+                    break;
+                default:  // DESC_PLANO ascending 
+                    planos = planos.OrderBy(s => s.DESC_PLANO);
+                    break;
+            }
+            #endregion ordenacao
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(planos.ToPagedList(pageNumber, pageSize));
+        }
+
+        //
+        // GET: /Plano/PlanoDelete/5
+        public ActionResult PlanoDelete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //throw new InvalidOperationException("Something very bad happened while doing important stuff");
+                throw new Exception();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Erro na exclusão. Tente novamente ou, se o problema persistir, entre em contato com o suporte.";
+            }
+
+            var plano = _planoAppService.Get(id ?? default(int));
+
+            if (plano == null)
+            {
+                //return HttpNotFound();
+                throw new Exception();
+            }
+
+            PlanoViewModel planoVM = new PlanoViewModel(plano);
+
+            return View(planoVM);
+        }
+
+        //POST: /Plano/PlanoDelete/5
+        [HttpPost, ActionName("PlanoDelete")]
+        public ActionResult PlanoDeleteConfirmed(int id)
+        {
+            try
+            {
+                var plano = _planoAppService.Get(id);
+
+                if (plano.PlanoCategorias.Count() == 0 && plano.DolarMedios.Count() == 0)
+                {
+                    _planoAppService.Remove(plano);
+                }
+            }
+            catch (RetryLimitExceededException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("PlanoDelete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("PlanoDelete");
+
         }
 
         protected override void Dispose(bool disposing)
--- Dufry.Comissoes/ViewModels/PlanoViewModel.cs
+++ Dufry.Comissoes/ViewModels/PlanoViewModel.cs
@@ -9,22 +9,22 @@ namespace Dufry.Comissoes.ViewModels
     {
         public Plano Plano { get; set; }
 
-        public Categoria Categoria { get; set; }
+        //public Categoria Categoria { get; set; }
 
-        public CategoriaPercentual CategoriaPercentual { get; set; }
+        //public CategoriaPercentual CategoriaPercentual { get; set; }
 
-        public PlanoCategoria PlanoCategoria { get; set; }
+        //public PlanoCategoria PlanoCategoria { get; set; }
 
-        public IEnumerable<SelectListItem> CategoriasSelectListItem { get; set; }
+        //public IEnumerable<SelectListItem> CategoriasSelectListItem { get; set; }
 
-        public IEnumerable<SelectListItem> LojasSelectListItem { get; set; }
+        //public IEnumerable<SelectListItem> LojasSelectListItem { get; set; }
 
-        public PlanoViewModel(Plano plano, IEnumerable<SelectListItem> categoriaSelectListItem, IEnumerable<SelectListItem> lojaSelectListItem)
-        {
-            Plano = plano;
-            CategoriasSelectListItem = categoriaSelectListItem;
-            LojasSelectListItem = lojaSelectListItem;
-        }
+        //public PlanoViewModel(Plano plano, IEnumerable<SelectListItem> categoriaSelectListItem, IEnumerable<SelectListItem> lojaSelectListItem)
+        //{
+        //    Plano = plano;
+        //    CategoriasSelectListItem = categoriaSelectListItem;
+        //    LojasSelectListItem = lojaSelectListItem;
+        //}
 
         public PlanoViewModel(Plano plano)
         {
