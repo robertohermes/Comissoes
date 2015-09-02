adadddb9af2f43af489d691007c5684f5aabf6ff
Roberto Hermes <robertohermes@gmail.com>
2015-09-02 18:05:45 -0300

    Atualização de versão de pacotes

--- Dufry.Comissoes/App_Start/NinjectWebCommon.cs
+++ Dufry.Comissoes/App_Start/NinjectWebCommon.cs
@@ -13,20 +13,20 @@ namespace Dufry.Comissoes.App_Start
     using Ninject;
     using Ninject.Web.Common;
 
-    public static class NinjectWebCommon 
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -34,7 +34,7 @@ namespace Dufry.Comissoes.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -64,6 +64,6 @@ namespace Dufry.Comissoes.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-        }        
+        }
     }
 }
