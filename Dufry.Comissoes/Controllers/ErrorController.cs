80d4b41d764b6baa78518367833d8b2846606426
Roberto Hermes <robertohermes@gmail.com>
2015-09-03 11:56:43 -0300

    Ajustes na estrutura de página de erro

---
+++ Dufry.Comissoes/Controllers/ErrorController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dufry.Comissoes.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //<REVER> Já existe um redirecionamento para a página "Shared/Error". Ajustar para usar só um.
+        // GET: /Error/
+        public ActionResult Index()
+        {
+            return View("Error");
+        }
+
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 200;
+            return View("NotFound");
+        }
+
+        public ActionResult InternalServer()
+        {
+            Response.StatusCode = 200;
+            return View("InternalServer");
+        }
+
+	}
+}
