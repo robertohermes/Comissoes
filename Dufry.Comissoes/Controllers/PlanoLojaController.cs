c0861d1ffe5a4d347f13e5f48b6e5a91260c25ed
Roberto Hermes <robertohermes@gmail.com>
2015-09-04 13:50:36 -0300

    Ajustes na criação e edição de Planos de Comissões por Lojas

--- Dufry.Comissoes/Controllers/PlanoLojaController.cs
+++ Dufry.Comissoes/Controllers/PlanoLojaController.cs
@@ -104,6 +104,252 @@ namespace Dufry.Comissoes.Controllers
             return View(planoloja);
         }
 
+        // GET: /PlanoLoja/PlanoLojaEdit/5
+        public ActionResult PlanoLojaEdit(int? id)
+        {
+            if (id == null)
+            {
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                throw new Exception();
+            }
+            var planoloja = _planolojaAppService.Get(id ?? default(int));
+            if (planoloja == null)
+            {
+                //return HttpNotFound();
+                throw new Exception();
+            }
+
+            #region populaobjetos
+            var planos = _planoAppService.All();
+            IEnumerable<SelectListItem> planosSelectListItem = new SelectList(planos, "ID_PLANO", "DESC_PLANO");
+            ViewBag.ID_PLANO = new SelectList(planos, "ID_PLANO", "DESC_PLANO", planoloja.ID_PLANO);
+
+            var lojas = _lojaAppService.Find(t => t.CodigoLojaAlternate.Trim() != "-2" && t.CodigoLojaAlternate.Trim() != "-1");
+            IEnumerable<SelectListItem> lojasSelectListItem = new SelectList(lojas, "CodigoLojaAlternate", "NomeLoja");
+            ViewBag.CODIGOLOJAALTERNATE = new SelectList(lojas, "CodigoLojaAlternate", "NomeLoja", planoloja.CODIGOLOJAALTERNATE);
+            #endregion populaobjetos
+
+            PlanoLojaViewModel planoLojaVM = new PlanoLojaViewModel(planoloja, planosSelectListItem, lojasSelectListItem);
+
+            return View(planoLojaVM);
+
+        }
+
+        // POST: /PlanoLoja/PlanoLojaEdit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost, ActionName("PlanoLojaEdit")]
+        //[ValidateAntiForgeryToken]
+        public ActionResult PlanoLojaEditConfirmed(int? id)
+        {
+
+            if (id == null)
+            {
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                throw new Exception();
+            }
+
+            var planolojaToUpdate = _planolojaAppService.Get(id ?? default(int));
+
+            planolojaToUpdate = ObtemPlanoLojaForm(planolojaToUpdate);
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    PlanoLoja planolojaExiste = new PlanoLoja();
+                    planolojaExiste = null;
+
+                    if (planolojaToUpdate.STATUS == "A")
+                    {
+                        planolojaExiste = PlanoLojaAtivaVigente(planolojaToUpdate);
+                    }
+
+                    if (planolojaExiste == null || planolojaToUpdate.STATUS == "I")
+                    {
+                        _planolojaAppService.Update(planolojaToUpdate);
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Já existe um perído vigente e ativo que coincide com a tentativa de inclusão / alteração");
+                    }
+
+                    return RedirectToAction("PlanoLojaIndex");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Erro na alteração. Tente novamente ou, se o problema persistir, entre em contato com o suporte.");
+                }
+            }
+
+            return View(planolojaToUpdate);
+        }
+
+                // GET: /PlanoLoja/PlanoLojaIndex
+        //[ControleAcessoAdminFilter]
+        public ViewResult PlanoLojaIndex(int? page
+            , string sortOrder
+            , int? idPlanoSearchString
+            , string codigolojaalternateSearchString
+            , string dtiniSearchString
+            , string dtfimSearchString
+            , string statusSearchString)
+        {
+
+            #region populaobjetos
+            var planos = _planoAppService.All();
+            ViewBag.idPlanoSearchString = new SelectList(planos, "ID_PLANO", "DESC_PLANO", idPlanoSearchString);
+
+            var lojas = _lojaAppService.Find(t => t.CodigoLojaAlternate.Trim() != "-2" && t.CodigoLojaAlternate.Trim() != "-1"); ;
+            ViewBag.codigolojaalternateSearchString = new SelectList(lojas, "CodigoLojaAlternate", "NomeLoja", codigolojaalternateSearchString);
+            #endregion populaobjetos
+
+            #region trataParametrosOrdenacao
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.DescCategoriaSortParam = String.IsNullOrEmpty(sortOrder) ? "DESC_CATEGORIA_desc" : "";
+            ViewBag.CodigoLojaAlternateSortParam = sortOrder == "NomeLoja" ? "NomeLoja_desc" : "NomeLoja";
+            ViewBag.DtIniSortParam = sortOrder == "DT_INI" ? "DT_INI_desc" : "DT_INI";
+            ViewBag.DtFimSortParam = sortOrder == "DT_FIM" ? "DT_FIM_desc" : "DT_FIM";
+            ViewBag.StatusSortParam = sortOrder == "STATUS" ? "STATUS_desc" : "STATUS";
+            #endregion trataParametrosOrdenacao
+
+            #region trataParametrosBusca
+
+            var predicate = PredicateBuilder.True<PlanoLoja>();
+
+            if (idPlanoSearchString.HasValue)
+            {
+                int idCategoriaFilter = idPlanoSearchString.GetValueOrDefault();
+                predicate = predicate.And(i => i.ID_PLANO.Equals(idCategoriaFilter));
+                ViewBag.idCategoriaFilter = idCategoriaFilter;
+            }
+
+            if (!String.IsNullOrEmpty(codigolojaalternateSearchString))
+            {
+                string codigolojaalternateFilter = codigolojaalternateSearchString;
+                predicate = predicate.And(i => i.CODIGOLOJAALTERNATE.Equals(codigolojaalternateFilter));
+                ViewBag.codigolojaalternateFilter = codigolojaalternateFilter;
+            }
+
+            if (!String.IsNullOrEmpty(dtiniSearchString))
+            {
+                DateTime dtiniFilter = DateTime.ParseExact(dtiniSearchString, "dd/MM/yyyy", new CultureInfo("pt-BR"));
+                predicate = predicate.And(i => i.DT_INI.Equals(dtiniFilter));
+                ViewBag.dtiniFilter = dtiniFilter;
+            }
+
+            if (!String.IsNullOrEmpty(dtfimSearchString))
+            {
+                DateTime dtfimFilter = DateTime.ParseExact(dtfimSearchString, "dd/MM/yyyy", new CultureInfo("pt-BR"));
+                predicate = predicate.And(i => i.DT_FIM.Equals(dtfimFilter));
+                ViewBag.dtfimFilter = dtfimFilter;
+            }
+
+            if (!String.IsNullOrEmpty(statusSearchString))
+            {
+                predicate = predicate.And(i => i.STATUS.Equals(statusSearchString));
+                ViewBag.statusFilter = statusSearchString;
+            }
+
+            #endregion trataParametrosBusca
+
+            IEnumerable<PlanoLoja> planolojas = new List<PlanoLoja>();
+
+            planolojas = _planolojaAppService.Find(predicate);
+
+            #region ordenacao
+            switch (sortOrder)
+            {
+                case "NomeLoja":
+                    planolojas = planolojas.OrderBy(s => s.CODIGOLOJAALTERNATE); //mudar de chave para campo
+                    break;
+                case "DT_INI":
+                    planolojas = planolojas.OrderBy(s => s.DT_INI); //mudar de chave para campo
+                    break;
+                case "DT_FIM":
+                    planolojas = planolojas.OrderBy(s => s.DT_FIM); //mudar de chave para campo
+                    break;
+                case "STATUS":
+                    planolojas = planolojas.OrderBy(s => s.STATUS);
+                    break;
+                case "DESC_CATEGORIA_desc":
+                    planolojas = planolojas.OrderByDescending(s => s.ID_PLANO); //mudar de chave para campo
+                    break;
+                case "NomeLoja_desc":
+                    planolojas = planolojas.OrderByDescending(s => s.CODIGOLOJAALTERNATE); //mudar de chave para campo
+                    break;
+                case "DT_INI_desc":
+                    planolojas = planolojas.OrderByDescending(s => s.DT_INI); //mudar de chave para campo
+                    break;
+                case "DT_FIM_desc":
+                    planolojas = planolojas.OrderByDescending(s => s.DT_FIM); //mudar de chave para campo
+                    break;
+                case "STATUS_desc":
+                    planolojas = planolojas.OrderByDescending(s => s.STATUS);
+                    break;
+                default:  // DESC_CATEGORIA ascending 
+                    planolojas = planolojas.OrderBy(s => s.ID_PLANO);
+                    break;
+            }
+            #endregion ordenacao
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(planolojas.ToPagedList(pageNumber, pageSize));
+        }
+
+        //
+        // GET: /PlanoLoja/PlanoLojaDelete/5
+        public ActionResult PlanoLojaDelete(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                //return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                //throw new InvalidOperationException("Something very bad happened while doing important stuff");
+                throw new Exception();
+            }
+
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Erro na exclusão. Tente novamente ou, se o problema persistir, entre em contato com o suporte.";
+            }
+
+            var planoloja = _planolojaAppService.Get(id ?? default(int));
+
+            if (planoloja == null)
+            {
+                //return HttpNotFound();
+                throw new Exception();
+            }
+
+            //return View(planoloja);
+            Loja loja = _lojaAppService.Find(t => t.CodigoLojaAlternate == planoloja.CODIGOLOJAALTERNATE).FirstOrDefault();
+
+            PlanoLojaViewModel categoriaPercentualVM = new PlanoLojaViewModel(planoloja, loja);
+            
+            return View(categoriaPercentualVM);
+        }
+
+        //POST: /PlanoLoja/PlanoLojaDelete/5
+        [HttpPost, ActionName("PlanoLojaDelete")]
+        public ActionResult PlanoLojaDeleteConfirmed(int id)
+        {
+            try
+            {
+                var planoloja = _planolojaAppService.Get(id);
+
+               _planolojaAppService.Remove(planoloja);
+            }
+            catch (RetryLimitExceededException/* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                return RedirectToAction("PlanoLojaDelete", new { id = id, saveChangesError = true });
+            }
+            return RedirectToAction("PlanoLojaIndex");
+
+        }
+
         private PlanoLoja PlanoLojaAtivaVigente(PlanoLoja pl)
         {
 
