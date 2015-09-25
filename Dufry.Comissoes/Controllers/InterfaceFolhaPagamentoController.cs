080d906d6a4b334eeac4297b8f8b1d371b01e609
Roberto Hermes <robertohermes@gmail.com>
2015-09-25 16:45:02 -0300

    Implementação inicial da estrutura de Interface com Folha de Pagamento

---
+++ Dufry.Comissoes/Controllers/InterfaceFolhaPagamentoController.cs
@@ -0,0 +1,65 @@
+﻿using Dufry.Comissoes.Application.Interfaces;
+using Dufry.Comissoes.Controllers.Helpers;
+using Dufry.Comissoes.Domain.Entities;
+using Dufry.Comissoes.Filters;
+using Dufry.Comissoes.ViewModels;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dufry.Comissoes.Controllers
+{
+    [ControleAcessoAdminFilter]
+    [HandleError(ExceptionType = typeof(InvalidOperationException),
+            View = "InvalidOperation")]
+    [HandleError(ExceptionType = typeof(HttpException),
+                View = "HttpException")]
+    public class InterfaceFolhaPagamentoController : Controller
+    {
+        private readonly IControleAcessoAppService _controleacessoAppService;
+        private readonly IEmpresaAppService _empresaAppService;
+        private readonly ILojaAppService _lojaAppService;
+
+        public InterfaceFolhaPagamentoController(IControleAcessoAppService controleacessoAppService, IEmpresaAppService empresaAppService, ILojaAppService lojaAppService)
+        {
+            _controleacessoAppService = controleacessoAppService;
+            _empresaAppService = empresaAppService;
+            _lojaAppService = lojaAppService;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _controleacessoAppService.Dispose();
+            _empresaAppService.Dispose();
+            _lojaAppService.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+
+        // GET: /Interface/InterfaceFolhaPagamentoIndex
+        public ActionResult InterfaceFolhaPagamentoIndex()
+        {
+            #region populaobjetos
+            var empresas = _empresaAppService.Find(t => t.CodigoEmpresaAlternate.Trim() != "-2" && t.CodigoEmpresaAlternate.Trim() != "-1"); ;
+            IEnumerable<SelectListItem> empresasSelectListItem = new SelectList(empresas, "CodigoEmpresaAlternate", "NomeEmpresa");
+            ViewBag.COLABORADORKEY = new SelectList(empresas, "CodigoEmpresaAlternate", "NomeEmpresa");
+
+            var lojas = _lojaAppService.Find(t => t.CodigoLojaAlternate.Trim() != "-2" && t.CodigoLojaAlternate.Trim() != "-1"); ;
+            IEnumerable<SelectListItem> lojasSelectListItem = new SelectList(lojas, "CodigoLojaAlternate", "NomeLoja");
+            ViewBag.CODIGOLOJAALTERNATE = new SelectList(lojas, "CodigoLojaAlternate", "NomeLoja");
+
+            #endregion populaobjetos
+
+            InterfaceFolhaPagamentoViewModel ifpVM = new InterfaceFolhaPagamentoViewModel(empresasSelectListItem, lojasSelectListItem);
+
+            return View(ifpVM);
+        }
+
+    }
+}
---
+++ Dufry.Comissoes/ViewModels/InterfaceFolhaPagamentoViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Dufry.Comissoes.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+//using AutoMapper;
+
+namespace Dufry.Comissoes.ViewModels
+{
+    public class InterfaceFolhaPagamentoViewModel
+    {
+        [Required(ErrorMessage = "A data inicial da vigência é obrigatória")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date, ErrorMessage = "A data inicial da vigência está em formato inválido")]
+        [Display(Name = "Data inicial da vigência")]
+        public DateTime DT_INI { get; set; }
+
+
+        [Required(ErrorMessage = "A data final da vigência é obrigatória")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [DataType(DataType.Date, ErrorMessage = "A data final da vigência está em formato inválido")]
+        [Display(Name = "Data final da vigência")]
+        public DateTime DT_FIM { get; set; }
+
+        public ControleAcesso ControleAcesso { get; set; }
+
+        public Empresa Empresa { get; set; }
+
+        public Loja Loja { get; set; }
+
+        public IEnumerable<SelectListItem> EmpresasSelectListItem { get; set; }
+
+        public IEnumerable<SelectListItem> LojasSelectListItem { get; set; }
+
+        public InterfaceFolhaPagamentoViewModel(IEnumerable<SelectListItem> empresasSelectListItem, IEnumerable<SelectListItem> lojasSelectListItem)
+        {
+            EmpresasSelectListItem = empresasSelectListItem;
+            LojasSelectListItem = lojasSelectListItem;
+        }
+
+
+    }
+}
