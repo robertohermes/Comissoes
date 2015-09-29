104a7b9c43c9c1f0834f1aba1863720c7c5ff009
Roberto Hermes <robertohermes@gmail.com>
2015-09-29 14:02:52 -0300

    Ajustes na busca de Planos de Comissões por Lojas

--- Dufry.Comissoes.Domain/Entities/Search/PlanoLojaSearch.cs
+++ Dufry.Comissoes.Domain/Entities/Search/PlanoLojaSearch.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 
-namespace Dufry.Comissoes.Domain.Entities
+namespace Dufry.Comissoes.Domain.Entities.Search
 {
     public partial class PlanoLojaSearch
     {
